933d4cf1b7d93c3276095f2123f376339a258455
Zack Webber <zack2.webber@live.uwe.ac.uk>
2026-04-21 17:03:58 +0100

    Another fix to error handling that came up after attempting to run from command line

--- main.c
+++ main.c
@@ -2,8 +2,11 @@
 #include "io.h"
 
 int main(int argc, char *argv[]) {      //values to allow function to accept an argument stored as an array of characters
-    if (argc < 2) return 1;             //only expecting 2 arguments of argc, one for the program name and one for the argument (filename input)
-
+    if (argc < 2)             //only expecting 2 arguments of argc, one for the program name and one for the argument (filename input)
+    {
+        printf("CSV file could not be found\n");
+        return 1;            //checking if filename has been entered into the command argument
+    }
     results values;                        //called for functions below that need to assign data to it
 
     RMS_voltage(&values, argv[1]);         //these function are returning values being assigned to the results structure so need to be called here and given the filename in form argv[1]
